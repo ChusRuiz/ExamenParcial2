98357de9f680fc74cdad6a621ea54135fc62f2f6
Jesús Ruiz <82255976+ChusRuiz@users.noreply.github.com>
2023-05-07 00:55:59 -0600

    Add files via upload

---
+++ parcial2-springboot-main/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into task (tittle, Description) values ('estereo', 'si');
+insert into task (tittle, Description) values ('TV', 'no');
+insert into task (tittle, Description) values ('LAPTOP', 'si');
+insert into task (tittle, Description) values ('Estufa', 'no');
